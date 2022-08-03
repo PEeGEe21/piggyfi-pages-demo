a0dc21866d2446eb6f0a0d477fe66fe3c8907ed4
Pr1j <95676395+Pr1j@users.noreply.github.com>
2022-08-03 11:30:13 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+        colors: {
+            primary: '#000042',
+            secondary: '#6E65D9',
+            tertiary: '#C6A538',
+        }
+        }
+    }
+    }
+
+
+    // $(document).ready(function(){
+        
+    // })
+    
