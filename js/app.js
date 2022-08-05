63acaf0bbcdada899c2d5d022883da436097d05a
Pr1j <95676395+Pr1j@users.noreply.github.com>
2022-08-05 15:22:38 +0100

    change

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ tailwind.config = {
             primary: '#000042',
             secondary: '#6E65D9',
             tertiary: '#C6A538',
+            border_cl: '#F5F5F6'
         }
         }
     }
