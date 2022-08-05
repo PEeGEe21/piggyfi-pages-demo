6e69c6ccc47e92689d78b0d91db661b4a2d2382c
= <=>
2022-08-06 00:33:10 +0100

    added dashboard

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@ tailwind.config = {
             primary: '#000042',
             secondary: '#6E65D9',
             tertiary: '#C6A538',
-            border_cl: '#F5F5F6'
+            border_cl: '#F5F5F6',
+            gray_cl: '#9998A9',
         }
         }
     }
