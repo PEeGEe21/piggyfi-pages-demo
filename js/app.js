360d8b4c2ea521a7f3bb62c17f27a48bd2a80f60
= <=>
2022-08-06 19:52:05 +0100

    made a lot of changes

--- js/app.js
+++ js/app.js
@@ -4,13 +4,39 @@ tailwind.config = {
         colors: {
             primary: '#000042',
             secondary: '#6E65D9',
+            secondary50: '#F1F0FB',
+            secondary500: '#F7CE46',
             tertiary: '#C6A538',
             border_cl: '#F5F5F6',
             gray_cl: '#9998A9',
+            neutral100: '#E6E5EA',
+            neutral700: '#4A4957',
+            neutral700: '#4A4957',
+
         }
         }
     }
     }
+
+//     background: #4A4957;
+// background: #9998A9;
+// background: #6E65D9;
+// background: #6F6E83;
+// background: #25252C;
+// background: #D6D6DD;
+// background: #F5F5F6;
+// background: #E6E5EA;
+// background: #F1F0FB;
+// background: #FFFFFF;
+// background: #45CD85;
+// background: #FFFFFF4D;
+// background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.35) 85.42%);
+// background: #00000036;
+// background: #35D07F;
+// background: #5EA33B;
+// background: #E7EFF3;
+// background: #F5F5F699;
+// background: #FBCC5C;
 
 
     // $(document).ready(function(){
